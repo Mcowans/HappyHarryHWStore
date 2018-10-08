55e8800ceb13e8fc5e0eaed28137ef463518e1c6
jperez25 <jova9991@gmail.com>
2018-10-08 11:17:06 -0500

    sign in working

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,6 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable, :rememberable,
+         :recoverable, :validatable, #:rememberable,
          :confirmable, :lockable
 end
